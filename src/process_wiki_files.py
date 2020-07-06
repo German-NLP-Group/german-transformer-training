2991463d6348be3683466a44219c10f481362783
PhilipMay <eniak.info@gmail.com>
2020-07-06 18:57:03 +0200

    Sentence splitting without token

---
+++ src/process_wiki_files.py
@@ -0,0 +1,143 @@
+from somajo import SoMaJo
+import os
+from multiprocessing import Pool, cpu_count
+
+INPUT_DIR = "../data"
+
+OUTPUT_DIR = "../output"
+
+tokenizer = SoMaJo("de_CMC")
+
+
+# see https://github.com/tsproisl/SoMaJo/issues/17
+def detokenize(tokens, line):
+    out = []
+    print_line = False
+    for token in tokens:
+        if token.original_spelling is not None:
+            token_str = token.original_spelling
+        else:
+            token_str = token.text
+
+        if not (token_str.startswith('<') and token_str.endswith('>')):
+            out.append(token_str)
+        else:
+            print(token_str)
+            print_line = True
+
+        if token.space_after:
+            out.append(" ")
+
+    result = "".join(out)
+
+    if print_line:
+        print('ori:', line)
+        print('result:', result)
+        print('#############################')
+    return result
+
+
+def is_doc_start_line(line):
+    return line.startswith('<doc')
+
+
+def is_doc_end_line(line):
+    return line.startswith('</doc')
+
+
+def get_data_dirs(root_dir):
+    result = []
+    for _, d_, _ in os.walk(root_dir):
+        for dir__ in d_:
+            result.append(dir__)
+    return result
+
+
+def process_text_line(line):
+    sentences = tokenizer.tokenize_text([line])
+
+    result = []
+
+    for s in sentences:
+        sentence_string = detokenize(s, line)
+        result.append(sentence_string)
+
+    return result
+
+
+def process_directory(input_dir, output_file):
+    with open(os.path.join(OUTPUT_DIR, output_file), 'a') as output_file:
+
+        # to avoid new line at end of file
+        first_line_written = False
+
+        # r_=root, d_=directories, f_=files
+        for r_, _, f_ in os.walk(input_dir):
+            for file_ in f_:
+                next_input_file = os.path.join(r_, file_)
+                print("Reading file:", next_input_file)
+
+                with open(next_input_file, "r") as input_file:
+
+                    skip_next_line = False
+                    
+                    for line in input_file:
+
+                        # drop line with start tag
+                        if is_doc_start_line(line):
+                            skip_next_line = True
+                            continue
+
+                        # drop line with end tag
+                        if is_doc_end_line(line):
+                            continue
+
+                        # skip first line to skip headline
+                        if skip_next_line == True:
+                            skip_next_line = False
+                            continue
+
+                        # skip empty lines
+                        if len(line) <= 1:
+                            continue
+                        
+                        sentences = process_text_line(line)
+                        
+                        for sentence in sentences:
+
+                            # ignore blank lines and make sure that stuff like "\n" is also ignored:
+                            if len(sentence) > 2:
+
+                                if first_line_written:
+                                    output_file.write("\n")
+                                else:
+                                    first_line_written = True
+
+                                output_file.write(sentence)
+
+
+def pd(map_item):
+    """Wrap call to process_directory to be called by map function"""
+    input_dir, output_file = map_item
+    print("Creating:", output_file)
+    process_directory(input_dir, output_file)
+
+
+if __name__ == '__main__':
+    data_dirs = get_data_dirs(INPUT_DIR)
+
+    call_list = []
+    for dir_ in data_dirs:
+        call_item = [os.path.join(INPUT_DIR, dir_), dir_ + ".txt"]
+        call_list.append(call_item)
+
+    pool_size = cpu_count() * 2 * 2
+    print("pool_size:", pool_size)
+
+    # debug
+    pool_size = 1
+
+    with Pool(pool_size) as p:
+        p.map(pd, call_list)
+
+    print("Done!")
