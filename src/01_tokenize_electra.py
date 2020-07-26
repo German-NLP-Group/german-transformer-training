f0f2cb8b8c748a90323da028370b6b2b3ddb2997
PhilipMay <eniak.info@gmail.com>
2020-07-26 16:42:25 +0200

    electra tokenizer for vocab

--- src/01_tokenize_electra.py
+++ src/01_tokenize_electra.py
@@ -1,17 +1,30 @@
 import os
 from tokenizers import BertWordPieceTokenizer
+from pathlib import Path
 
-save_dir = "vocab_de"
-text_corpus_file = './corpus_dir/dewiki-talk-20200620.txt'
-vocab_size = 32_000
+save_dir = "vocab"
+paths = [str(x) for x in Path("./corpus/").glob("**/*.txt")]
+vocab_size = 32_767  # 2^15-1
 min_frequency = 2
 
 os.makedirs(save_dir, exist_ok=True)
-paths = [text_corpus_file]
+
+special_tokens = [
+    "[PAD]",
+    "[UNK]",
+    "[CLS]",
+    "[SEP]",
+    "[MASK]"]
+
+for i in range(200):
+    special_tokens.append('[unused{}]'.format(i))
 
 tokenizer = BertWordPieceTokenizer()
-tokenizer.train(files=paths, vocab_size=vocab_size,
-                min_frequency=min_frequency)
+tokenizer.train(files=paths, 
+                vocab_size=vocab_size,
+                min_frequency=min_frequency,
+                special_tokens=special_tokens,
+                )
 
 tokenizer.save_model(save_dir)
 tokenizer.save(save_dir + "/tokenizer.json")
