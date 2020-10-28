f73e8ff032b50bd2b15aaa04048c4c9f218647ff
Philipp Reißel <39518904+Phil1108@users.noreply.github.com>
2020-10-28 22:20:59 +0100

    Faster approach for dedup in mongodb

--- Hashing/Dedup.py
+++ Hashing/Dedup.py
@@ -33,50 +33,25 @@ def str_hash(s: str) -> int:
     h = hashlib.sha1(bytes(s, encoding="utf-8"))
     return h.hexdigest()
 
-if __name__ == '__main__': 
-    """
-    Info: 
-        approx: 2.8 Mio Documents per Part (4 GB)
-        Total: Approx 60-80 Parts: 
-            => 180 Mio Documents
-    
-    """
-    path = Path("/media/data/48_BERT/Common_Crawl/S3_head_sync/2020-10")
-    file = Path("de_head_0000.json.gz")
-    total_path = path / file
-    
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.common_crawl
-    table = db.main
 
-    
-    #Can't create index due to too large keys
-    """
-    19 Mio Documents: 3 minutes
-    """
-    res = table.aggregate([{'$group': {'_id':'$url', 'count': {'$sum': 1}}}, {'$match': {'count': {'$gt': 1}}}], allowDiskUse=True)
-    a = list(res)
-    print(len(a))
-    
-    dup_urls = {part['_id']: part for part in a}
 
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.common_crawl
-    table_same_url = db.same_url
+def rewrite_file(path, outpath): 
+    """
+    Path: Objects to read_in file and folder to output
+    Reads in file
+    Writes content to new file but without duplicates
     
-    
-    #Create Index
-    resp = table.create_index([ ("new_hashes", -1) ])
-    
+    Returns name of txt file (which needs to be compressed)
+    """
     counter = 0
-    outfile = open('output.txt', 'w')
+    out_total_path = outpath / Path(path.stem)
+    out_txt_file = str(out_total_path).split('.')[0] + '.txt'
+    outfile = open(out_txt_file, 'w')
     DROP_KEYS = ['title', 'raw_content']
-    with gzip.open(total_path,'rt') as f:
+    with gzip.open(path,'rt') as f:
         """
         Check if line is still in database
-        
         """
-        _tmp_lines = []
         for index, line in tqdm(enumerate(f)):
             Drop = False
             line_dict = eval(line)
@@ -85,13 +60,17 @@ if __name__ == '__main__':
             res = table.find({'digest' : line_dict['digest'], 
                               'cc_segment': line_dict['cc_segment'], 
                               'date_download': line_dict['date_download']})
+            #pdb.set_trace()
+            if index == 1000000: 
+                break
             if len(list(res)) > 0: 
-                
+                None
+                """
                 #Only useful for duplicate urls
                 if line_dict['url'] in dup_urls:
                     
                     line_dict['new_hashes'] = str_hash(line_dict['raw_content'])
-                    
+                    pdb.set_trace()
                     res = table_same_url.find({'new_hashes': line_dict['new_hashes'],
                                      'url': line_dict['url']})
                     nr_res = len(list(res))            
@@ -104,6 +83,7 @@ if __name__ == '__main__':
                         for key in DROP_KEYS: 
                             line_dict.pop(key)
                         table_same_url.insert_one(line_dict)
+                """
             #If it is not in main mongodb then it was a duplicate and is removed
             else: 
                 Drop = True
@@ -111,18 +91,63 @@ if __name__ == '__main__':
             if not Drop: 
                 if 'new_hashes' in line_dict.keys(): 
                     line_dict.pop('new_hashes' )
-                outfile.write(str(line_dict))
-                
+                outfile.write(str(line_dict))     
     outfile.close()
+    return Path(out_txt_file)
 
-    tar = tarfile.open("output.tar.gz", "w:gz")
-    tar.add('output.txt')
-    tar.close()
+if __name__ == '__main__': 
+    """
+    Info: 
+        approx: 2.8 Mio Documents per Part (4 GB)
+        Total: Approx 60-80 Parts: 
+            => 180 Mio Documents
     
-    print(counter)
+    """
     
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.common_crawl
+    table = db.main
+    table_same_url = db.same_url
+
     
-    table.find()
+    #Can't create index due to too large keys
+    """
+    19 Mio Documents: 3 minutes
+    """
+    
+    """
+    res = table.aggregate([{'$group': {'_id':'$url', 'count': {'$sum': 1}}}, 
+                           {'$match': {'count': {'$gt': 1}}}], allowDiskUse=True)
+    a = list(res)
+    print(len(a))
+    
+    dup_urls = {part['_id']: part for part in a}
+    """
+    
+    resp = table.create_index([ ("new_hashes", -1) ])
+    
+    """
+    Rewrite Files from here
+    """
+    path = Path("/media/data/48_BERT/Common_Crawl/S3_head_sync")
+    for cc_folder in path.iterdir():
+        """
+        parallelize here
+        """
+        for in_file in cc_folder.iterdir():
+            if in_file.suffix == '.gz' and 'head' in in_file.name:
+                outpath = Path('data/')
+                
+                file_txt = rewrite_file(in_file, outpath)
+            
+                file_tar_gz = str(file_txt.parents[0]) + '_cleaned.tar.gz'
+                tar = tarfile.open(file_tar_gz, "w:gz")
+                tar.add(file_txt)
+                tar.close()
+        break
+        
+        #pdb.set_trace()
+    
     
     
     
--- Hashing/Insert2Mongo.py
+++ Hashing/Insert2Mongo.py
@@ -14,30 +14,74 @@ import pickle
 from pymongo import MongoClient
 from tqdm import tqdm 
 from collections import Counter
-import ray 
+from typing import Iterable, Iterator, Sequence, Sized, Type
+import numpy as np 
+import hashlib 
 
-def add2mongo(path, table): 
+HASH_TYPE: Type[np.uint64] = np.uint64
+HASH_SIZE = HASH_TYPE(0).nbytes
+
+def _b2i(b: bytes) -> int:
+    return np.frombuffer(b[:HASH_SIZE], dtype=HASH_TYPE, count=1, offset=0).item(0)
+
+def str_hash(s: str) -> int:
+    h = hashlib.sha1(bytes(s, encoding="utf-8"))
+    return h.hexdigest()
+
+
+def add2mongo(path): 
     """
     Inserts metadata to mongodb
     path: Path to tar.gz file from CC Dataset
     table: Mongodb table
     """
-    DROP_KEYS = ['title', 'raw_content']
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.common_crawl
+    table = db.main
+    
+    DROP_KEYS = ['title', 'raw_content', 'length', 'nlines', 'language', 'source_domain', 'original_nlines']
     with gzip.open(path,'rt') as f:
         _tmp_lines = []
         for index, line in tqdm(enumerate(f)):
             line_dict = eval(line)
+            line_dict['new_hashes'] = str_hash(line_dict['raw_content'])
             for key in DROP_KEYS: 
                 line_dict.pop(key)
             
+            #Adds new hashes
+            
             _tmp_lines.append(line_dict)
-            if index % 1000 == 0 and index > 0: 
+            if index % 5000 == 0 and index > 0: 
                 table.insert_many(_tmp_lines)
                 _tmp_lines = []
-    
+                
+    #One Time at the end
+    table.insert_many(_tmp_lines)
+    _tmp_lines = []
+        
     return None 
 
 
+
+def add_cc_month(path): 
+    """
+    Adds complete common crawl month 
+    """
+    programms = []
+    for file in path.iterdir(): 
+        print(file.suffix)
+        if file.suffix == '.gz' and 'head' in file.name:
+            print(f'Adding lines from {file}')
+            p = Process(target=add2mongo, args=(file,))
+            p.start()
+            programms.append(p)
+            with open('finished.txt', 'a') as f: 
+                f.write(str(file) + '\n')
+    
+    for p in programms: 
+        p.join()
+        
+    return True
 
 if __name__ == '__main__': 
     """
@@ -53,25 +97,24 @@ if __name__ == '__main__':
     """
     from multiprocessing import Process
     
-    path = Path("/media/data/48_BERT/Common_Crawl/S3_head_sync/2020-10")
-    
     client = MongoClient('mongodb://localhost:27017/')
     db = client.common_crawl
     table = db.main
     
-    resp = table.create_index([ ("digest", -1) ])    
+
     
-    programms = []
-    for file in path.iterdir(): 
-        print(file.suffix)
-        if file.suffix == '.gz':
-            p = Process(target=add2mongo, args=(file, table))
-            p.start()
-            programms.append(p)
     
-    for p in programms: 
-        p.join()
+    #path = Path("/media/data/48_BERT/Common_Crawl/S3_head_sync")
+    path = Path("/home/ubuntu/dedup/s3")
+    for folder in path.iterdir(): 
+        _ = add_cc_month(folder)
+        #pdb.set_trace()
     
+    """
+    To-Do: 
+        Call pymongo connection only inside multithreaded funciton
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    """
     
     """
     19,953,008 22:06
@@ -80,21 +123,31 @@ if __name__ == '__main__':
     -> 19,075,556 22:17
     
     Dedup happens here
+    
+    BOTTLENECK !!!!
+    53 Mio 
     """
     
-    pdb.set_trace()
+    #resp = table.create_index([ ("digest", -1) ])   
+    resp = table.create_index([ ("new_hashes", -1) ])
     
+    #pdb.set_trace()
+    #pdb.set_trace()
+    print(table.count())
+    print('Starting Deduplication')
+    import time 
+    start_time = time.time()
     table.aggregate(    [ 
         { "$sort": { "_id": 1 } }, 
         { "$group": { 
-            "_id": "$digest", 
+            "_id": "$new_hashes", 
             "doc": { "$first": "$$ROOT" } 
         }}, 
         { "$replaceRoot": { "newRoot": "$doc" } },
         { "$out": "main" }
     ], 
     allowDiskUse=True )
-    
-
-    
+    end_time = time.time()
+    print(end_time - start_time)
+    print(table.count())
     
